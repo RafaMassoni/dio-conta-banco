bbc9ccb0f08a087ce68496216212b7685eb9c5fb
rafael.ministro <rafael.ministro@vilt-group.com>
2025-06-09 16:04:38 -0300

    ✨ Criando projeto

--- src/Main.java
+++ src/Main.java
@@ -22,8 +22,8 @@ public class Main {
         double saldo = scanner.nextDouble();
 
 
-        ContaTerminal contaTerminal1 = new ContaTerminal(numeroConta, numeroAgencia, nomeCliente, new BigDecimal(saldo));
-        contaTerminal1.printContaCriadaComSucesso();
+        ContaTerminal contaTerminal = new ContaTerminal(numeroConta, numeroAgencia, nomeCliente, new BigDecimal(saldo));
+        contaTerminal.printContaCriadaComSucesso();
 
         scanner.close();
 
